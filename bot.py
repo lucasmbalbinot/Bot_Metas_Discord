e0a13e57b5b53cd327925add80f137cd1c02dff2
lucasmbalbinot <61811436+lucasmbalbinot@users.noreply.github.com>
2024-06-04 03:23:24 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 # Substitua 'your_token_here' pelo token do seu bot
-TOKEN = 'MTI0NzQzMzQ2MzA1ODAxMDE3NA.GwXcIG.mXvu206MlClM8xGCdcVFyH5G_9u6Cr0yMtNpNE'
+TOKEN = 'MTI0NzQzMzQ2MzA1ODAxMDE3NA.G_DxNZ.yhthu3t9VX1e3JeScfYtL0LywDZWx6dTE5JaCU'
 
 # Configurações do bot
 intents = discord.Intents.default()
